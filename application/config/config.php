2bad16acc06ac3bd295c896703acda0596ef9818
dyapz <japsalonga@gmail.com>
2024-07-18 11:25:10 +0800

    removed reCaptcha Token

--- application/config/config.php
+++ application/config/config.php
@@ -530,6 +530,3 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-
-$config['google_key'] = '6LdzfDgkAAAAAEXD-3-M_Gf32pkG-3etINv1qGOn';
-$config['google_secret'] = '6LdzfDgkAAAAAA04K-s4ISKinOFPoNObh7wDRUsA';
