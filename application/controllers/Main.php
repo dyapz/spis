10e5d39c585e2c028753514f015490fb3d08e5b7
dyapz <japsalonga@gmail.com>
2024-08-07 11:14:53 +0800

    update as of 8-7-2024 11:14am

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -27,24 +27,10 @@ class Main extends CI_Controller {
 		}else{
 
 			$this->load->view('template/header');
-			$this->load->view('template/nav');
 			$this->load->view('index');
 			$this->load->view('template/footer');
 		}
 	}
-
-	public function client_list(){
-		if(!$this->session->userdata('user_type')){ 
-            $this->session->set_flashdata("error", "Sorry you don't have permission to access the page you were trying to reach!");
-			redirect('auth');
-		}else{
-		$this->load->view('template/header');
-		$this->load->view('template/nav');
-		$this->load->view('client_list');
-		$this->load->view('template/footer');
-		}
-	}
-
 
 	public function newuser(){
 		$this->load->view('newuser');
